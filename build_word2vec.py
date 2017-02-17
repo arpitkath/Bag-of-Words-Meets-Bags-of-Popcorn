4ce1780f21746df8139c7bfe17703d8a6de00c3a
arpitkath <arpit.kathuria07@gmail.com>
2017-02-17 14:54:01 +0530

    fixed UnicodeDecodeError..again

--- build_word2vec.py
+++ build_word2vec.py
@@ -1,19 +1,26 @@
+# -*- coding: utf-8 -*- 
 import pandas as pd
 from bs4 import BeautifulSoup
 import re
 from nltk.corpus import stopwords
 import nltk.data
 from gensim.models import word2vec
+import string
 
-train = pd.read_csv( "labeledTrainData.tsv", header=0, delimiter="\t", quoting=3 )
-test = pd.read_csv( "testData.tsv", header=0, delimiter="\t", quoting=3 )
-unlabeled_train = pd.read_csv( "unlabeledTrainData.tsv", header=0, delimiter="\t", quoting=3 )
+train = pd.read_csv( "labeledTrainData.tsv", header=0, delimiter="\t", quoting=3, encoding="utf-8" )
+test = pd.read_csv( "testData.tsv", header=0, delimiter="\t", quoting=3, encoding="utf-8" )
+unlabeled_train = pd.read_csv( "unlabeledTrainData.tsv", header=0, delimiter="\t", quoting=3, encoding="utf-8" )
+
+def clean_query(s):
+	#s = re.sub(r'[^\w]', '', s)
+	#s = re.sub(r'[^\x00-\x7F]+','', s)
+	return ''.join([i if ord(i) < 128 else ' ' for i in s])
 
 print "Data loaded."
 # Will return a list of words in the sentence after removing HTML tags, non alphabets, and converting them to lower case.
 def review2list( review ):
 
-	text = BeautifulSoup(review).get_text().decode('utf-8').strip()
+	text = BeautifulSoup(review, 'html').get_text().strip()
 	text = re.sub("[^a-zA-Z]", "", text)
 	words = text.lower().split()
 	stop_words = set(stopwords.words("english"))
@@ -23,9 +30,9 @@ def review2list( review ):
 # Will return a list of sentences extracted from paragraph.
 def review2sentences( review, tokenizer ):
 
-	_sentences = tokenizer.tokenize(review.decode('utf-8').strip())
+	_sentences = tokenizer.tokenize(review.strip())
 	sentences = []
-	for sentence in sentences:
+	for sentence in _sentences:
 		if len(sentence) > 0:
 			sentences.append(review2list(sentence))
 
@@ -35,11 +42,11 @@ sentences = []
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 print "Chunking sentences."
 for sentence in train["review"]:
-	sentences.extend(review2sentences(sentence, tokenizer))
+	sentences.extend(review2sentences(clean_query(sentence), tokenizer))
 
 for sentence in unlabeled_train["review"]:
 	sentences.extend(review2sentences(sentence, tokenizer))
-
+print len(sentences), "sentences loaded."
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
     level=logging.INFO)
