4b705bc672df4b0f1a7fbffa07af1b2934bbdef2
arpitkath <arpit.kathuria07@gmail.com>
2017-02-17 13:46:12 +0530

    to build word vectors

---
+++ build_word2vec.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import re
+from nltk.corpus import stopwords
+import nltk.data
+from gensim.models import word2vec
+
+nltk.download()
+
+train = pd.read_csv( "labeledTrainData.tsv", header=0, delimiter="\t", quoting=3 )
+test = pd.read_csv( "testData.tsv", header=0, delimiter="\t", quoting=3 )
+unlabeled_train = pd.read_csv( "unlabeledTrainData.tsv", header=0, delimiter="\t", quoting=3 )
+
+
+# Will return a list of words in the sentence after removing HTML tags, non alphabets, and converting them to lower case.
+def review2list( review ):
+
+	text = BeautifulSoup(review).get_text()
+	text = re.sub("[^a-zA-Z]", "", text)
+	words = text.lower().split()
+	stop_words = set(stopwords.words("english"))
+	words = [word for word in words if word not in stop_words]
+	return words
+
+# Will return a list of sentences extracted from paragraph.
+def review2sentences( review, tokenizer ):
+
+	_sentences = tokenizer.tokenize(review.strip())
+	sentences = []
+	for sentence in sentences:
+		if len(sentence) > 0:
+			sentences.append(review2list(sentence))
+
+	return sentences
+
+sentences = []
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+for sentence in train["review"]:
+	sentences.extend(review2sentences(sentence, tokenizer))
+
+for sentence in unlabeled_train["review"]:
+	sentences.extend(review2sentences(sentence, tokenizer))
+
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
+    level=logging.INFO)
+
+model = word2vec.Word2Vec(sentences, workers=4, size=200, min_count=50, window=10, downsampling=1e-3)
+model.init_sims(replace=True)
+model.save("bowmbop_word2vec")
