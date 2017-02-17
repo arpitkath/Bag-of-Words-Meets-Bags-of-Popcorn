6678b1472855d16453893306de4605c4be55ad50
arpitkath <arpit.kathuria07@gmail.com>
2017-02-17 14:08:07 +0530

    fixed UnicodeDecodeError

--- build_word2vec.py
+++ build_word2vec.py
@@ -5,17 +5,15 @@ from nltk.corpus import stopwords
 import nltk.data
 from gensim.models import word2vec
 
-nltk.download()
-
 train = pd.read_csv( "labeledTrainData.tsv", header=0, delimiter="\t", quoting=3 )
 test = pd.read_csv( "testData.tsv", header=0, delimiter="\t", quoting=3 )
 unlabeled_train = pd.read_csv( "unlabeledTrainData.tsv", header=0, delimiter="\t", quoting=3 )
 
-
+print "Data loaded."
 # Will return a list of words in the sentence after removing HTML tags, non alphabets, and converting them to lower case.
 def review2list( review ):
 
-	text = BeautifulSoup(review).get_text()
+	text = BeautifulSoup(review).get_text().decode('utf-8').strip()
 	text = re.sub("[^a-zA-Z]", "", text)
 	words = text.lower().split()
 	stop_words = set(stopwords.words("english"))
@@ -25,7 +23,7 @@ def review2list( review ):
 # Will return a list of sentences extracted from paragraph.
 def review2sentences( review, tokenizer ):
 
-	_sentences = tokenizer.tokenize(review.strip())
+	_sentences = tokenizer.tokenize(review.decode('utf-8').strip())
 	sentences = []
 	for sentence in sentences:
 		if len(sentence) > 0:
@@ -35,7 +33,7 @@ def review2sentences( review, tokenizer ):
 
 sentences = []
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-
+print "Chunking sentences."
 for sentence in train["review"]:
 	sentences.extend(review2sentences(sentence, tokenizer))
 
